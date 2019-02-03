b750b58b30a62cebd0f1c66a503abca171a93539
sajibsarkar <thebapi@gmail.com>
2019-02-03 19:30:54 +0600

    config updated

--- install.sh
+++ install.sh
@@ -6,9 +6,4 @@ run /pd_build/utilities.sh
 run /pd_build/python.sh
 run /pd_build/golang.sh
 
-
-
-# Must be installed after Ruby, so that we don't end up with two Ruby versions.
-run /pd_build/nginx-passenger.sh
-
 run /pd_build/finalize.sh
