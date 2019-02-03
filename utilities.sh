f8423f2289697d320a206f914c7bc42e36e23b26
sajibsarkar <thebapi@gmail.com>
2019-02-03 19:28:29 +0600

    config updated

--- utilities.sh
+++ utilities.sh
@@ -3,7 +3,7 @@
 set -e
 source /pd_build/buildconfig
 
-run apt-get update && apt-get upgrade
+run apt-get -y update && apt-get -y upgrade
 ## Many Ruby gems and NPM packages contain native extensions and require a compiler.
 run minimal_apt_get_install build-essential
 ## Bundler has to be able to pull dependencies from git.
